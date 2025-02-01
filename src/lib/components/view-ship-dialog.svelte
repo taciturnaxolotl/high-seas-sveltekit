8733a1383f7de4d8922fc43b40fb7a543ba74acc
Mahad Kalam <mahadkalam1@proton.me>
2025-02-01 01:14:03 +0000

    WIP ship dialog

--- src/lib/components/view-ship-dialog.svelte
+++ src/lib/components/view-ship-dialog.svelte
@@ -2,16 +2,29 @@
   import { Dialog, Label, Separator } from "bits-ui";
   import { fade } from "svelte/transition";
   import { flyAndScale } from "$lib/utils";
-  import Button from "$lib/components/button.svelte";
   import X from "lucide-svelte/icons/x";
-  import { onMount } from "svelte";
   import type { ShipGroup } from "$lib/server/data";
+  import { micromark } from "micromark";
+  import { gfmHtml, gfm } from "micromark-extension-gfm";
+  import sanitizeHtml from "sanitize-html";
 
   // biome-ignore lint/style/useConst: cannot bind to a constant in Svelte
   let {
     open = $bindable(false),
     shipGroup = $bindable(),
   }: { open: boolean; shipGroup: ShipGroup } = $props();
+
+  async function fetchReadme() {
+    const latestShip = shipGroup.ships[shipGroup.ships.length - 1];
+    const response = await fetch(latestShip.readmeUrl);
+    const md = await response.text();
+    return sanitizeHtml(
+      micromark(md, {
+        extensions: [gfm()],
+        htmlExtensions: [gfmHtml()],
+      })
+    );
+  }
 </script>
 
 <Dialog.Root bind:open>
@@ -33,6 +46,17 @@
 
       <!-- Dialog content -->
       <div class="flex flex-col items-start gap-4 pb-2 pt-4">
+        {#await fetchReadme()}
+          <div class="bg-surface0 p-4 rounded shadow">Loading...</div>
+        {:then md}
+          <div class="prose max-w-full text-text">
+            {@html md}
+          </div>
+        {:catch error}
+          <div class="bg-surface0 p-4 rounded shadow">
+            Error: {error.message}
+          </div>
+        {/await}
         <Dialog.Close
           class="absolute right-5 top-5 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-surface1 focus-visible:ring-offset-[2px] focus-visible:ring-offset-surface0 active:scale-98"
         >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,65 @@
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import catppuccin from '@catppuccin/tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
-import type { Config } from 'tailwindcss';
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import catppuccin from "@catppuccin/tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
+
+const accent = "text";
+const linkColor = "mauve";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+
+            "--tw-prose-body": theme("colors.text.DEFAULT"),
+            "--tw-prose-headings": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-lead": theme("colors.text.DEFAULT"),
+            "--tw-prose-links": theme(`colors.${linkColor}.DEFAULT`),
+            "--tw-prose-bold": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-counters": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-bullets": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-hr": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-quotes": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-quote-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-captions": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-code": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-pre-code": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-pre-bg": theme("colors.base.DEFAULT"),
+            "--tw-prose-th-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-td-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-body": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-bullets": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-hr": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-quotes": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-quote-borders": theme(
+              `colors.${accent}.DEFAULT`
+            ),
+            "--tw-prose-invert-captions": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-td-borders": theme(`colors.${accent}.DEFAULT`),
+          },
+        },
+      }),
       fontFamily: {
-        sans: ['Inter Variable', ...fontFamily.sans],
+        sans: ["Inter Variable", ...fontFamily.sans],
       },
     },
   },
 
-  plugins: [typography, forms, catppuccin({ defaultFlavour: "mocha" })]
+  plugins: [typography, forms, catppuccin({ defaultFlavour: "mocha" })],
 } satisfies Config;
